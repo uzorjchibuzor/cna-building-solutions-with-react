c58f9c485c09607be9d46b92b394e56770fd73f5
Uzor Efedigbue <cefedigbuej@gmail.com>
2020-10-12 12:15:31 +0100

    complete the cart feature for the application

--- src/shop/Shop.js
+++ src/shop/Shop.js
@@ -4,6 +4,11 @@ import { ProductList } from "./ProductList";
 import { CartSummary } from "./CartSummary";
 
 export class Shop extends Component {
+
+  handleAddToCart = (...args) => {
+    this.props.addToCart(...args);
+    this.props.history.push("/shop/cart");
+  }
   render() {
     return (
       <div className="container-fluid">
@@ -22,7 +27,7 @@ export class Shop extends Component {
           </div>
           <div className="col-9 p-2">
             <ProductList products={this.props.products} 
-                addToCart={ this.props.addToCart } />
+                addToCart={ this.handleAddToCart } />
           </div>
         </div>
       </div>
--- src/shop/ShopConnector.js
+++ src/shop/ShopConnector.js
@@ -10,6 +10,7 @@ import {
   removeFromCart,
   clearCart
 } from "../data/CartActionCreators";
+import { CartDetails } from './CartDetails'
 
 const mapStateToProps = dataStore => ({
   ...dataStore
@@ -46,6 +47,7 @@ export const ShopConnector = connect(
               />
             )}
           />
+          <Route path="/shop/cart" render={ (routeProps) => <CartDetails { ...this.props } { ...routeProps } /> } />
           <Redirect to="/shop/products" />
         </Switch>
       );
